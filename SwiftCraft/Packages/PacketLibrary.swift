1f621fdd1a4d2fe7780ea35e0adb11966764ca76
Noah Peeters <noah.peeters@icloud.com>
2018-05-26 14:20:57 +0200

    Add plugin message packet

--- SwiftCraft/Packages/PacketLibrary.swift
+++ SwiftCraft/Packages/PacketLibrary.swift
@@ -44,6 +44,7 @@ public struct DefaultPacketLibrary: PacketLibrary {
         // Play
         ReceiveChatMessagePacket.self,
         ReceiveMultiBlockChangePacket.self,
+        PluginMessageReceivedPacket.self,
         ReceiveMultiBlockChangePacket.self,
         KeepaliveRequestPacket.self,
         JoinGamePacket.self,
---
+++ SwiftCraft/Packages/Play/Clientbound/PluginMessageReceivedPacket.swift
@@ -0,0 +1,27 @@
+//
+//  PluginMessageReceivedPacket.swift
+//  SwiftCraft
+//
+//  Created by Noah Peeters on 26.05.18.
+//  Copyright © 2018 Noah Peeters. All rights reserved.
+//
+
+import Foundation
+
+/// Packet send to transmitt plugin message.
+public struct PluginMessageReceivedPacket: ReceivedPacket {
+    public static var packetID = PacketID(connectionState: .play, id: 0x18)
+
+    /// The string identifying the channel.
+    ///
+    /// - Note: Minecraft internal channels are prefixed with "MC|".
+    public let channel: String
+
+    /// The data send over the channel.
+    public let data: ByteArray
+
+    public init<Buffer: ReadBuffer>(from buffer: Buffer) throws where Buffer.Element == Byte {
+        channel = try String(from: buffer)
+        data = buffer.readRemainingElements()
+    }
+}
