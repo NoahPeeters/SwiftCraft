f44a0fb07c288bb2c030bbd0def7c6564abe11d7
Noah Peeters <noah.peeters@icloud.com>
2018-06-28 22:11:54 +0200

    Add DisconnectPlayPacket id test

--- Tests/SwiftCraftTests/Packets Tests/DisconnectPlayPacketTests.swift
+++ Tests/SwiftCraftTests/Packets Tests/DisconnectPlayPacketTests.swift
@@ -13,26 +13,33 @@ import Nimble
 
 public class DisconnectPlayPacketTests: QuickSpec {
     public override func spec() {
-        describe("deserializing with protocol version 340") {
-            var packet: DisconnectPlayPacket?
+        context("checking with protocol version 340") {
+            describe("deserializing") {
+                var packet: DisconnectPlayPacket?
 
-            beforeEach {
-                packet = try? DisconnectPlayPacket(
-                    from: [
-                        16, 0x7b, 0x22, 0x74, 0x65, 0x78,
-                        0x74, 0x22, 0x3a, 0x22, 0x48, 0x65,
-                        0x6c, 0x6c, 0x6f, 0x22, 0x7d
-                    ],
-                    context: MockSerializationContext.play340)
+                beforeEach {
+                    packet = try? DisconnectPlayPacket(
+                        from: [
+                            16, 0x7b, 0x22, 0x74, 0x65, 0x78,
+                            0x74, 0x22, 0x3a, 0x22, 0x48, 0x65,
+                            0x6c, 0x6c, 0x6f, 0x22, 0x7d
+                        ],
+                        context: MockSerializationContext.play340)
+                }
+
+                it("succeds") {
+                    expect(packet).toNot(beNil())
+                }
+
+                it("has the correct message") {
+                    let message = packet?.message.message.string(translationManager: EnglishTranslationManager())
+                    expect(message).to(equal("Hello"))
+                }
             }
 
-            it("succeds") {
-                expect(packet).toNot(beNil())
-            }
-
-            it("has the correct message") {
-                let message = packet?.message.message.string(translationManager: EnglishTranslationManager())
-                expect(message).to(equal("Hello"))
+            it("has the correct packet id") {
+                expect(DisconnectPlayPacket.packetID(context: MockSerializationContext.play340))
+                    .to(equal(PacketID(connectionState: .play, id: 0x1A)))
             }
         }
     }
